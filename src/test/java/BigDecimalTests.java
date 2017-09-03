e4d5ec8f3bae798aba256dae29c7456931fd3e8f
Pawel M <pawmac24@gmail.com>
2017-09-04 01:31:43 +0200

    adding test

--- src/test/java/BigDecimalTests.java
+++ src/test/java/BigDecimalTests.java
@@ -16,4 +16,13 @@ public class BigDecimalTests {
         BigDecimal expectedResult = new BigDecimal(0.2957).setScale(4, BigDecimal.ROUND_HALF_DOWN);
         assertEquals(expectedResult, result);
     }
+
+    @Test
+    public void testDivideBigDecimal2() {
+        double numerator = 29.57;
+        double denominator = 100;
+        BigDecimal result = new BigDecimal(numerator / denominator).setScale(4, BigDecimal.ROUND_HALF_DOWN);
+        BigDecimal expectedResult = new BigDecimal(0.2957).setScale(4, BigDecimal.ROUND_HALF_DOWN);
+        assertEquals(expectedResult, result);
+    }
 }
