ba3d2927c674d23a936036c01cb6c0853a0f24f5
Pawel M <pawmac24@gmail.com>
2017-09-04 00:42:32 +0200

    first calculation - polish

--- src/main/java/demo/service/FundMoneyDivisionCalculatorServiceImpl.java
+++ src/main/java/demo/service/FundMoneyDivisionCalculatorServiceImpl.java
@@ -5,6 +5,7 @@ import demo.model.FundType;
 import demo.model.InvestmentFund;
 import demo.model.profiles.InvestmentProfile;
 import demo.model.profiles.SafeInvestmentProfile;
+import org.apache.commons.collections.ArrayStack;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -19,14 +20,29 @@ public class FundMoneyDivisionCalculatorServiceImpl implements FundMoneyDivision
     public List<FundAmountOfMoneyDivision> divideAmountOfMoney(BigDecimal investmentMoney,
                                                                InvestmentProfile investmentProfile,
                                                                List<InvestmentFund> investmentFunds) {
-        System.out.println(investmentMoney);
-        System.out.println("SafeInvestmentProfile = " + (investmentProfile instanceof SafeInvestmentProfile));
+
+        List<FundAmountOfMoneyDivision> fundAmountOfMoneyDivisionList = new ArrayList<>();
 
         List<InvestmentFund> polishFundList = investmentFunds
                 .stream()
                 .filter(i -> i.getType().equals(FundType.POLISH))
                 .collect(Collectors.toList());
-        System.out.println("polishFundList size = " + polishFundList.size());
+        BigDecimal fundListSize = new BigDecimal(polishFundList.size()).setScale(4, BigDecimal.ROUND_HALF_DOWN);
+        for(InvestmentFund investmentFund : polishFundList){
+            FundAmountOfMoneyDivision fundAmountOfMoneyDivision = new FundAmountOfMoneyDivision();
+            fundAmountOfMoneyDivision.setFundType(investmentFund.getType());
+            fundAmountOfMoneyDivision.setFundName(investmentFund.getName());
+            BigDecimal dividedMoney = investmentMoney.multiply(investmentProfile.getPercentPolish())
+                    .divide(fundListSize, 4, BigDecimal.ROUND_HALF_DOWN)
+                    .setScale(0, BigDecimal.ROUND_HALF_DOWN);
+            fundAmountOfMoneyDivision.setDividedMoney(dividedMoney);
+            BigDecimal dividedPercent = investmentProfile.getPercentPolish()
+                    .divide(fundListSize, 4, BigDecimal.ROUND_HALF_DOWN)
+                    .setScale(4, BigDecimal.ROUND_HALF_DOWN);
+            fundAmountOfMoneyDivision.setDividedPercent(dividedPercent);
+            System.out.println(fundAmountOfMoneyDivision.toString());
+            fundAmountOfMoneyDivisionList.add(fundAmountOfMoneyDivision);
+        }
 
         List<InvestmentFund> foreignFundList = investmentFunds
                 .stream()
@@ -40,6 +56,6 @@ public class FundMoneyDivisionCalculatorServiceImpl implements FundMoneyDivision
                 .collect(Collectors.toList());
         System.out.println("moneyFundList size = " + moneyFundList.size());
 
-        return new ArrayList<>();
+        return fundAmountOfMoneyDivisionList;
     }
 }
